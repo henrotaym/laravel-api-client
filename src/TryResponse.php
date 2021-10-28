f76aa9ea49f5f2152242146e79f9c8dcf0f823b7
Mathieu Henrotay <mathieu.henrotay@gmail.com>
2021-10-28 16:41:37 +0200

    🚧 implementing try/catch response.

---
+++ src/TryResponse.php
@@ -0,0 +1,89 @@
+<?php
+namespace Henrotaym\LaravelApiClient;
+
+use Henrotaym\LaravelApiClient\Contracts\ResponseContract;
+use Henrotaym\LaravelApiClient\Contracts\TryResponseContract;
+use Henrotaym\LaravelApiClient\Exceptions\RequestRelatedException;
+
+class TryResponse implements TryResponseContract
+{
+    /**
+     * Error after trying the request.
+     * 
+     * @var RequestRelatedException|null
+     */
+    protected $error;
+
+    /**
+     * Response after trying request.
+     * 
+     * @return ResponseContract|null
+     */
+    protected $response;
+
+    /**
+     * Error after trying the request.
+     * 
+     * @return RequestRelatedException|null Null if there is no exception.
+     */
+    public function error(): ?RequestRelatedException
+    {
+        return $this->error;
+    }
+
+    /**
+     * Setting up error.
+     * 
+     * @param RequestRelatedException $error
+     * @return TryResponseContract
+     */
+    public function setError(RequestRelatedException $error): TryResponseContract
+    {
+        $this->error = $error;
+
+        return $this;
+    }
+
+    /**
+     * Response after trying the request.
+     * 
+     * @return ResponseContract|null Null if there is no response.
+     */
+    public function response(): ?ResponseContract
+    {
+        return $this->response;
+    }
+
+    /**
+     * Setting up response.
+     * 
+     * @param ResponseContract $response
+     * @return TryResponseContract
+     */
+    public function setResponse(ResponseContract $response): TryResponseContract
+    {
+        $this->response = $response;
+
+        return $this;
+    }
+
+    /**
+     * Telling if request failed.
+     * 
+     * @return bool True if failed.
+     */
+    public function failed(): bool
+    {
+        return !$this->ok();
+    }
+
+    /**
+     * Telling if request was ok.
+     * 
+     * @return bool True if successful
+     */
+    public function ok(): bool
+    {
+        return $this->error && $this->response->ok();
+    }
+}
